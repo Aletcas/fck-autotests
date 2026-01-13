67cc364f0ccb6215f6165c46c31b9aa137172601
Aletca <aletca@gmail.com>
2026-01-13 21:24:04 +0300

    FooterTest

--- src/test/java/autotest/steps/FooterTest.java
+++ src/test/java/autotest/steps/FooterTest.java
@@ -2,6 +2,12 @@ package autotest.steps;
 
 import autotest.config.PlaywrightConfig;
 import autotest.pages.PageObject;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static autotest.locators.Locators.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class FooterTest {
@@ -14,4 +20,45 @@ public class FooterTest {
         this.playwrightConfig = playwrightConfig;
     }
 
+    @Given("I open the Playwright footer page")
+    public void iOpenThePlaywrightTestPage() {
+        pageObject
+                .getPage()
+                .navigate(playwrightConfig.getTestPageUrl());
+    }
+
+    @When("I check for the footer")
+    public void iCheckForTheFooter() {
+        boolean footerExists = pageObject
+                .getPage().locator(FOOTER_SELECTOR)
+                .count() > 0;
+
+        assertTrue(footerExists, "Футер должен присутствовать на странице");
+
+        }
+    @Then("I verify the footer is displayed")
+    public void iVerifyTheFooterIsDisplayed() {
+        boolean isFooterVisible = pageObject
+                .getPage()
+                .locator(FOOTER_SELECTOR)
+                .first()
+                .isVisible();
+
+        assertTrue(isFooterVisible, "Футер должен быть видимым");
+
+        boolean hasContactLink = pageObject
+                .getPage()
+                .locator(CONTACT_LINK_SELECTOR)
+                .count() > 0;
+
+        assertTrue(hasContactLink, "Ссылка 'Контактная информация' должна присутствовать в футере");
+
+        // Проверка наличия ссылки "Политика конфиденциальности"
+        boolean hasPrivacyLink = pageObject
+                .getPage()
+                .locator(PRIVACY_POLICY_LINK_SELECTOR)
+                .count() > 0;
+
+        assertTrue(hasPrivacyLink, "Ссылка 'Политика конфиденциальности' должна присутствовать в футере");
+    }
 }
